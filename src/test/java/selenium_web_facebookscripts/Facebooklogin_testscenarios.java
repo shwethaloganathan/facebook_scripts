a0ce6f74af76364080ba22a3543b68c4349fa78c
sakiv <sakiv@10.0.0.147>
2025-01-19 21:25:44 -0500

    my selenium first scripts

---
+++ src/test/java/selenium_web_facebookscripts/Facebooklogin_testscenarios.java
@@ -0,0 +1,18 @@
+package selenium_web_facebookscripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Facebooklogin_testscenarios {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.name("email")).sendKeys("shwetha");
+		driver.findElement(By.name("pass")).sendKeys("apple123");
+		driver.findElement(By.name("login")).click();
+	}
+
+}
